94b3b046031204d6d9cb5302121b73d3e85c6e6e
Rishi6jp <rainbro187@gmail.com>
2025-07-23 10:14:09 +0530

    aad

--- src/sections/Schedule.tsx
+++ src/sections/Schedule.tsx
@@ -1,16 +1,22 @@
+"use client"
+
 import { Button } from "@/components/Button"
 import deadLift from "@/styles/assets/DeadLift.jpg"
 import Image from "next/image"
 import squat from "@/styles/assets/images.png"
 import timetable from "@/styles/assets/schedule.png"
+import { useState } from "react"
+import {motion, spring} from "framer-motion"
 
 
 
 export const Schedule = () =>{
+    const [toggle, setToggle] = useState(false)
+
     return <div className="bg-black text-white pb-20 pt-20 space-y-20">
         <div className="mx-50 text-center space-y-3">
             <div>
-                <Image src={timetable}></Image>
+                <Image width="0" height="0" alt="" src={timetable}></Image>
             </div>
             <div><Button title="VIEW MORE"/></div>
         </div>
@@ -19,9 +25,25 @@ export const Schedule = () =>{
         </div>
         <div className="text-center">
             <div className="tracking-wider text-5xl font-heading text-white">OUR GYM PASSES</div>
+            <div className="flex items-center flex-col justify-center space-y-2">
+                <div className="flex gap-2"><p className={toggle ? "text-white/50" : "text-white"}>Monthly</p> / <p className={toggle ? "text-white" : "text-gray-600"}>Yearly</p></div>
+                <motion.div
+                className="bg-yellow h-4 rounded-full w-9 flex items-center px-0.5"
+                onClick={() => setToggle(!toggle)}
+                >
+                    <motion.div
+                    initial={toggle? {translateX:0} : {translateX:18}}
+                    animate={toggle? {translateX:18}: {translateX: 0}}
+                    transition={{stiffness: 540,damping: 30, type: spring}}
+                    
+                    className="bg-black h-3.5 w-3.5 rounded-full"
+                    >
+
+                    </motion.div>
+                </motion.div>
+            </div>
             <div>
-                <div>Monthly / Yearly</div>
-                <button type="toggle">he</button>
+
             </div>
         </div>
         <div className="mx-50 border-grayer border-1"></div>
@@ -32,3 +54,7 @@ export const Schedule = () =>{
         </div>
     </div>
 }
+
+// const Plan = () => {
+
+// }
